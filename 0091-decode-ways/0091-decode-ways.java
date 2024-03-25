35f68022f7414e87ba2725064b00c3b0a2c38390
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-03-25 17:21:19 +0530

    Time: 1 ms (80.80%), Space: 41.9 MB (34.18%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,27 +1,23 @@
-public class Solution {
+class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        if (n == 0 || s.charAt(0) == '0') {
-            return 0;
-        }
+        if(n == 0 || s.charAt(0) =='0') return 0;
+        
+        int[] dp = new int[n+1];
 
-        // dp[i] represents the number of ways to decode the substring s[0:i]
-        int[] dp = new int[n + 1];
-        dp[0] = 1; // Base case: Empty string can be decoded in 1 way
-        dp[1] = 1; // First character can be decoded in 1 way
-
-        for (int i = 2; i <= n; i++) {
-            // Check if single digit is valid
-            if (s.charAt(i - 1) != '0') {
-                dp[i] += dp[i - 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n ;i++){
+            if(s.charAt(i-1) !='0'){
+                dp[i] += dp[i-1];
             }
-            // Check if two digits is valid
-            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
-            if (twoDigits >= 10 && twoDigits <= 26) {
-                dp[i] += dp[i - 2];
+
+            int twoDigit = Integer.parseInt(s.substring(i-2,i));
+            if(twoDigit >= 10 && twoDigit <=26){
+                dp[i] += dp[i-2];
             }
         }
-
         return dp[n];
     }
 }
