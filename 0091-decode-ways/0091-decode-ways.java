48f8d9cd5f1527d33c8bdb2217084b9881ac4203
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-03-25 17:16:05 +0530

    Time: 1 ms (80.80%), Space: 41.5 MB (79.91%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,20 +1,27 @@
-class Solution {
+public class Solution {
     public int numDecodings(String s) {
-        if (s.charAt(0) == '0') return 0;
-        if (s.length() == 1) return 1;
-        int n = s.length(), count = 0;
-        int[] dp = new int[3];
-        dp[0] = 1;
-        if (s.charAt(0) != '0') {
-            dp[1] = 1;
+        int n = s.length();
+        if (n == 0 || s.charAt(0) == '0') {
+            return 0;
         }
+
+        // dp[i] represents the number of ways to decode the substring s[0:i]
+        int[] dp = new int[n + 1];
+        dp[0] = 1; // Base case: Empty string can be decoded in 1 way
+        dp[1] = 1; // First character can be decoded in 1 way
+
         for (int i = 2; i <= n; i++) {
-            dp[i % 3] = 0;
-            if (s.charAt(i - 1) != '0') dp[i % 3] = dp[(i - 1) % 3];
-            if (s.charAt(i - 2) > '2' || s.charAt(i - 2) <= '0') continue;
-            if (s.charAt(i - 2) == '2' && s.charAt(i - 1) <= '6') dp[i % 3] += dp[(i - 2) % 3];
-            if (s.charAt(i - 2) == '1') dp[i % 3] += dp[(i - 2) % 3];
+            // Check if single digit is valid
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+            // Check if two digits is valid
+            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+            if (twoDigits >= 10 && twoDigits <= 26) {
+                dp[i] += dp[i - 2];
+            }
         }
-        return dp[n % 3];
+
+        return dp[n];
     }
 }
